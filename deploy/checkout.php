c35558e87be536aa2609e1ef07c3d2c260f7fa53
alexbash <bashalex87@yahoo.com>
2013-10-23 15:45:33 +0300

    Refactored git handlers

--- deploy/checkout.php
+++ deploy/checkout.php
@@ -5,20 +5,35 @@ ini_set('display_errors', 'On');
 set_time_limit(90);
 
 require_once('Git/Client.php');
-$git = new \Git\Client;
+try
+{
+    $git = new \Git\Client;
 
-chdir($_POST['directory']);
-$checkout = $git->checkout($_POST['branch']);
-chdir('../');
+    if(!is_dir($_POST['directory']))
+    {
+        throw new \Git\ClientException('Repository directory not exists. Please, try again to clone repository');
+    }
+    chdir($_POST['directory']);
+    $checkout = $git->checkout($_POST['branch']);
+    chdir('../');
 
-$destination = dirname(dirname(__FILE__)) . '/';
-$wwwDir = (empty($_POST['altDir'])) ? basename($_POST['branch']) : $_POST['altDir'];
-rename($_POST['directory'], $destination . $wwwDir);
+    $destination = dirname(dirname(__FILE__)) . '/';
+    $wwwDir = (empty($_POST['altDir'])) ? basename($_POST['branch']) : $_POST['altDir'];
+    if(is_dir($destination.$wwwDir))
+    {
+        throw new \Git\ClientException('Repository branch already exists');
+    }
+    rename($_POST['directory'], $destination . $wwwDir);
 
-$parts = explode('/', $_POST['branch']);
-$entry = trim(end($parts), '* ') . ':' . $wwwDir . PHP_EOL;
-file_put_contents('structure', $entry , FILE_APPEND|LOCK_EX);
-
-echo json_encode(array('status' => true));
-
-?>
+    $parts = explode('/', $_POST['branch']);
+    $entry = trim(end($parts), '* ') . ':' . $wwwDir . PHP_EOL;
+    file_put_contents('structure', $entry , FILE_APPEND|LOCK_EX);
+    $result = array('status' => 200, 'message' => 'Repository was cloned successfully');
+}
+catch(\Git\ClientException $e)
+{
+    $result = array('status' => 400, 'reason' => $e->getMessage());
+}
+header('Content-type: application/json');
+echo json_encode($result);
+exit();
--- deploy/clone.php
+++ deploy/clone.php
@@ -5,15 +5,21 @@ ini_set('display_errors', 'On');
 set_time_limit(90);
 
 require_once('Git/Client.php');
-$git = new \Git\Client;
-
-$repo = 'git@github.com:' . $_POST['repo'];
-$repo = $git->cloneRepo($repo);
-$branches = $git->branches($repo);
-
-echo json_encode(array(
-	'directory' => $repo,
-	'branches'  => $branches,
-));
-
-?>
+try
+{
+    $git = new \Git\Client;
+    $repo = 'git@github.com:' . $_POST['repo'];
+    $repo = $git->cloneRepo($repo);
+    $branches = $git->branches($repo);
+    $result = array('status' => 200, 'data' => array(
+        'directory' => $repo,
+        'branches'  => $branches,
+    ));
+}
+catch(\Git\ClientException $e)
+{
+    $result = array('status' => 400, 'reason' => $e->getMessage());
+}
+header('Content-type: application/json');
+echo json_encode($result);
+exit();
